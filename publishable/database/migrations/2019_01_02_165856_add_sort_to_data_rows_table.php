3f817c71e6b19b92e855d8eed15d22fb6b2ac350
Artem Hordin <temka.gordin@gmail.com>
2019-01-02 14:22:06 +0200

    add sorting table and permissions

---
+++ publishable/database/migrations/2019_01_02_165856_add_sort_to_data_rows_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppAddressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //Create new sorting permission
+
+        Schema::table('data_rows', function (Blueprint $table) {
+            $table->boolean('sort')->default(0)->after('delete');
+        });
+        //save id permission
+        $permission_list = [];
+        //create new permission
+        $permissions = Permission::whereNotNull('slug')->groupBy('slug')->get('slug');
+        foreach ($permissions as $permission) {
+            $new_permission = new Permission();
+            $new_permission->key = 'sort_' . $permission->slug;
+            $new_permission->slug = $permission->slug;
+            $new_permission->save();
+            $permission_list[] = $new_permission->id;
+        }
+        Schema::create('data_sort', function (Blueprint $table) {
+            $table->unsignedInteger('data_type_id');
+            $table->unsignedInteger('data_row_id');
+            $table->unsignedInteger('order');
+        });
+        /*     //insert permission to table
+             foreach ($permission_list as $permission_role){
+
+             }*/
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('data_rows', function (Blueprint $table) {
+            $table->dropColumn('sort');
+        });
+    }
+}
